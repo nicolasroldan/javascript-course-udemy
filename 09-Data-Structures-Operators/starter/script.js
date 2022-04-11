e74fb234a24610ccaf349a47d729a5580524350e
Nicolas Roldan <nicolasroldan@Air-de-Nicolas.fibertel.com.ar>
2022-04-11 16:09:17 -0300

    challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -108,84 +108,137 @@ const restaurant = {
 
 // Challenge 2
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-  printGoals: function(...players) {
-    for(let i = 0; i < players.length; i++) {
-      let goals = 0;
-      this.scored.forEach(el => {
-        if(el === players[i]) {
-          goals++;
-        }
-      })
-      console.log(`Player ${players[i]}, goals: ${goals}`);
-    }
-  }
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+//   printGoals: function(...players) {
+//     for(let i = 0; i < players.length; i++) {
+//       let goals = 0;
+//       this.scored.forEach(el => {
+//         if(el === players[i]) {
+//           goals++;
+//         }
+//       })
+//       console.log(`Player ${players[i]}, goals: ${goals}`);
+//     }
+//   }
+// };
+
+// // 1
+// for (const [index, player] of game.scored.entries()) {
+//   console.log(`Goal ${index + 1}: ${player}`);
+// }
+
+// // 2
+// let acumm = 0;
+// for (const odd of Object.values(game.odds)) {
+//   acumm += odd;
+// }
+//  acumm /= Object.values(game.odds).length;
+
+// console.log(acumm);
+
+// // 3 
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   console.log(`Odd of ${game[team] ? `wining ${game[team]}` : 'draw'}: ${odd}`);
+// }
+
+// // 4
+
+
+
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : scorers[player] = 1;
+// }
+
+// console.log(scorers)
+// let testMap = new Map()
+// testMap.set('abc', 'shsh')
+// console.log(testMap)
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
 
 // 1
-for (const [index, player] of game.scored.entries()) {
-  console.log(`Goal ${index + 1}: ${player}`);
-}
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
 
 // 2
-let acumm = 0;
-for (const odd of Object.values(game.odds)) {
-  acumm += odd;
-}
- acumm /= Object.values(game.odds).length;
 
-console.log(acumm);
+gameEvents.delete(64)
+console.log(gameEvents)
 
-// 3 
-for (const [team, odd] of Object.entries(game.odds)) {
-  console.log(`Odd of ${game[team] ? `wining ${game[team]}` : 'draw'}: ${odd}`);
-}
+// 3
+
+const average = 90 / gameEvents.size
+console.log(`An event happened, on average, every ${average} minutes`)
 
 // 4
 
-
-
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : scorers[player] = 1;
+for(const [minute, event] of gameEvents) {
+  console.log(`[${minute <= 45 ? 'FIRST': 'SECOND'} HALF] ${minute}: ${event}`)
 }
-
-console.log(scorers)
